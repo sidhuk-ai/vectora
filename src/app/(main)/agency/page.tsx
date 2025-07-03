4c12ee6f5480c12c9ee66402a738b4138278d534
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-07-03 22:36:46 +0530

    feat: add invitation verification and user details retrieval to agency page

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,4 +1,11 @@
-export default function Page() {
+import { getAuthUserDetails } from "@/actions/auth";
+import { verifyAndAcceptInvitation } from "@/actions/invitations";
+
+export default async function Page() {
+    const agencyId = await verifyAndAcceptInvitation();
+    console.log(agencyId);
+
+    const user = await getAuthUserDetails();
     return (
         <div>
             Agency page
