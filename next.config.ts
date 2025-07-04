24c60e5299d1891139c65806640c5d6451dbf19f
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-07-04 20:51:14 +0530

    feat: add remote image patterns to next.config for external image support

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'y8q7br14f8.ufs.sh'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
